776a1bd71602a99db6d45a86264b2dc7be357d11
Lucas Barboza <lpbarboza@gmail.com>
2014-08-28 16:24:37 -0300

    Add facebook login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,95 @@
 var express = require('express');
 var router = express.Router();
+var FB              = require('fb'),
+    Step            = require('step'),
+    config          = require('../config.js');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+FB.options({
+    appId:          config.facebook.appId,
+    appSecret:      config.facebook.appSecret,
+    redirectUri:    config.facebook.redirectUri
 });
 
+router.index = function(req, res) {
+    var accessToken = req.session.access_token;
+    if(accessToken) {
+        res.render('index', {
+            loginUrl: FB.getLoginUrl({ scope: 'user_about_me,publish_actions,manage_pages,read_insights,read_stream' })
+        });
+    } else {
+        res.redirect('/app');
+    }
+};
+
+router.loginCallback = function (req, res, next) {
+    var code            = req.query.code;
+
+    if(req.query.error) {
+        // user might have disallowed the app
+        return res.send('login-error ' + req.query.error_description);
+    } else if(!code) {
+        return res.redirect('/');
+    }
+
+    Step(
+        function exchangeCodeForAccessToken() {
+            FB.napi('oauth/access_token', {
+                client_id:      FB.options('appId'),
+                client_secret:  FB.options('appSecret'),
+                redirect_uri:   FB.options('redirectUri'),
+                code:           code
+            }, this);
+        },
+        function extendAccessToken(err, result) {
+            if(err) throw(err);
+            FB.napi('oauth/access_token', {
+                client_id:          FB.options('appId'),
+                client_secret:      FB.options('appSecret'),
+                grant_type:         'fb_exchange_token',
+                fb_exchange_token:  result.access_token
+            }, this);
+        },
+        function (err, result) {
+            if(err) return next(err);
+
+            req.session.access_token    = result.access_token;
+            req.session.expires         = result.expires || 0;
+
+            if(req.query.state) {
+                var parameters              = JSON.parse(req.query.state);
+                parameters.access_token     = req.session.access_token;
+
+                console.log(parameters);
+
+                FB.api('/me/' + config.facebook.appNamespace +':eat', 'post', parameters , function (result) {
+                    console.log(result);
+                    if(!result || result.error) {
+                        return res.send(500, result || 'error');
+                        // return res.send(500, 'error');
+                    }
+
+                    return res.redirect('/');
+                });
+            } else {
+                return res.redirect('/');
+            }
+        }
+    );
+};
+
+router.logout = function (req, res) {
+    req.session = null; // clear session
+    res.redirect('/');
+};
+
+/* GET home page. */
+router.get('/', router.index);
+
+/* GET login-facebook. */
+router.get('/loginCallback', router.loginCallback);
+
+/* GET login-facebook. */
+router.get('/logout', router.logout);
+
 module.exports = router;
+
