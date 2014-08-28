70d05f2ca7082e2f65728533f84c1ec1f46c5bed
Willian R. Sabião <willianjedai@gmail.com>
2014-08-28 16:24:36 -0300

    Lista de paginas proprias e de usuarios

--- public/js/dev/controllers/register-pages.js
+++ public/js/dev/controllers/register-pages.js
@@ -1,3 +1,13 @@
 app.controller('RegisterPagesCtrl', function ( $scope ) {
-  console.log('oi');
+  $scope.othersPages = [
+    { url: '', name: 'ScupBrasil' },
+    { url: '', name: 'Facebook' },
+    { url: '', name: 'Nintendo' },
+    { url: '', name: '9GAG' }
+  ];
+
+  $scope.addPage = function ( url ) {
+    $scope.othersPages.push({ name: $scope.urlPage });
+    $scope.urlPage = '';
+  }
 });
