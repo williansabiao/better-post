acb46b11feecdf736aaba9fc66b9d19397961fed
Lucas Barboza <lpbarboza@gmail.com>
2014-08-28 16:25:07 -0300

    Add fonts and config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"facebook" : {
+		"appId" : "1552370238316590",
+		"appSecret" : "ec3e7fed336ad9c44524abdf04c82f66",
+		"redirectUri" : "http://localhost:5000/loginCallback"
+	}
+};
